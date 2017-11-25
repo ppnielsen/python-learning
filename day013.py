bc376f5c4a865a8b91e78884c47499cd6fac1379
Philip Nielsen <philip.pj.nielsen@gmail.com>
2017-11-25 07:36:56 -0600

    adding day 13 to complete

--- day013.py
+++ day013.py
@@ -14,3 +14,45 @@ for row in range(0,7):
             letter_s = letter_s + ' '
     letter_s = letter_s + '\n'
 print(letter_s)
+
+# program to calculates age in dog years
+# dog year 0-2 = 10.5 human years
+# after that, each dog year = 4 human years
+
+human = int(input('How old are you?'))
+dog = 0
+
+if human < 0:
+    print('Age must be a positive integer!')
+elif human <= 2:
+    dog = human * 10.5
+else:
+    dog = 21 + ((human - 2) * 4)
+
+print(f'You are {dog} years old in dog years')
+
+
+# check to see if a letter is a consonant or vowel
+
+x = input('Please enter one letter from the alphabet: ')
+
+if x in ('a','e','i','o','u'):
+    print(f'{x} is a vowel')
+elif x == 'y':
+    print('A, E, I, O, U, and sometimes Y')
+else:
+    print(f'{x} is a consonant')
+
+# give a month and it'll tell you how many days
+thirty = ('April', 'June', 'September', 'November')
+thirty_one = ('January','March','May', 'July', 'August','October','December')
+mth = input('Please input a month: ')
+
+if mth == 'February':
+    print(f'{mth} has 28/29 days')
+elif mth in thirty:
+    print(f'{mth} has 30 days')
+elif mth in thirty_one:
+    print(f'{mth} has 31 days')
+else:
+    print(f'{mth} is not a valid month!')
